856b70f5f59894a8c8b2ac8c36696ff79fef1b97
Michael Ben-David <mikebd@users.noreply.github.com>
2023-11-27 10:39:56 -0500

    git.IsBehindRemote()

--- pkg/git/git.go
+++ pkg/git/git.go
@@ -1,8 +1,30 @@
 package git
 
-import "os/exec"
+import (
+	"github.com/mikebd/go-util/pkg/shell"
+	"os/exec"
+)
 
+// Fetch fetches the specified branch from the specified remote
+// for the repository in the current working directory
 func Fetch(remote string, branch string) error {
 	cmd := exec.Command("git", "fetch", remote, branch)
 	return cmd.Run()
+}
+
+// IsBehindRemote returns true if the specified branch is behind the specified remote
+// for the repository in the current working directory.
+func IsBehindRemote(remote string, branch string) (bool, error) {
+	fetchErr := Fetch(remote, branch)
+	if fetchErr != nil {
+		return false, fetchErr
+	}
+
+	cmd := exec.Command("git", "rev-list", "--count", branch+".."+remote+"/"+branch)
+	output, err := cmd.Output()
+	if err != nil {
+		return false, err
+	}
+
+	return shell.IsOutputGreaterThanZero(output), nil
 }
