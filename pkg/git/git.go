b128b7f8b61a8e261a20210c013ef7b4867ee262
Michael Ben-David <mikebd@users.noreply.github.com>
2023-11-27 20:24:08 -0500

    git.CurrentBranchName()

--- pkg/git/git.go
+++ pkg/git/git.go
@@ -5,6 +5,16 @@ import (
 	"os/exec"
 )
 
+func CurrentBranchName() (string, error) {
+	cmd := exec.Command("git", "rev-parse", "--abbrev-ref", "HEAD")
+	output, err := cmd.Output()
+	if err != nil || len(output) <= 1 {
+		return "", err
+	}
+
+	return string(output)[:len(output)-1], nil
+}
+
 // Fetch fetches the specified branch from the specified remote
 // for the repository in the current working directory
 func Fetch(remote string, branch string) error {
