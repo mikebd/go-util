b2194f9fe2aaaca16fa9f23afb17436464b0e2b5
Michael Ben-David <mikebd@users.noreply.github.com>
2025-04-27 21:10:59 -0400

    wip test with mock

--- pkg/pattern/behave/command/runner_default_test.go
+++ pkg/pattern/behave/command/runner_default_test.go
@@ -1,31 +1,59 @@
 package command
 
-import "testing"
+import (
+	"testing"
 
-var registry = func() *Registry {
-	result := NewRegistry("a registry")
-	result.Add("command1", func(_ []string) error { return nil })
-	return result
-}()
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
 
-var runner = DefaultRunner
+type MockCommand struct {
+	mock.Mock
+}
+
+// Execute satisfies the Command function signature
+func (m *MockCommand) Execute(args []string) error {
+	called := m.Called(args)
+	return called.Error(0)
+}
 
 func TestRunnerRun(t *testing.T) {
+	var mockCommand = func() *MockCommand {
+		result := MockCommand{}
+		result.On("Execute", []string{}).Return(nil)
+		result.On("Execute", []string{"arg1"}).Return(nil)
+		return &result
+	}()
+
+	var registry = func() *Registry {
+		result := NewRegistry("a registry")
+		result.Add("command1", mockCommand.Execute)
+		return result
+	}()
+
+	var runner = DefaultRunner
+
 	tests := []struct {
 		name    string
 		command string
 		args    []string
 		wantErr bool
 	}{
-		{"Valid command", "command1", []string{}, false},
+		{"Valid command - no args", "command1", []string{}, false},
+		{"Valid command - 1 arg", "command1", []string{"arg1"}, false},
 		{"Invalid command", "invalidCommand", []string{}, true},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			mockCommand.Calls = nil
 			err := runner.Run(registry, tt.command, tt.args...)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("runner.run() error = %v, wantErr %v", err, tt.wantErr)
+			if tt.wantErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+				mockCommand.AssertCalled(t, "Execute", tt.args)
+				mockCommand.AssertExpectations(t)
 			}
 		})
 	}
